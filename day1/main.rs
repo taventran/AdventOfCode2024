6f599cee7f9725df65687753ff8c65286e20ba85
taventran <taventran@gmail.com>
2024-12-01 22:15:46 -0600

    First day

---
+++ day1/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+fn bubble_sort(vec<i32> list1, vec<i32> list2) -> vec<i32> {
+
+}
+
+fn main() {
+    let input: String = fs::read_to_string("input.txt")
+        .expect("Could not read file");
+
+    println!("{input}")
+}
